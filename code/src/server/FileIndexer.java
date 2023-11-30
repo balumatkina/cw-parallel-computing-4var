9db6887aed23365beadc5ab01e4c65df8896789b
balumatkina <balumatkinaolga@gmail.com>
2023-11-30 14:33:37 +0200

    feat(FileIndexer): add inverted index filler

---
+++ code/src/server/FileIndexer.java
@@ -0,0 +1,46 @@
+package code.src.server;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileIndexer implements Runnable {
+
+    private Map<String, Set<String>> index;
+    private ArrayList<File> files;
+
+    FileIndexer(Map<String, Set<String>> index, ArrayList<File> files) {
+        this.index = index;
+        this.files = files;
+    }
+
+    @Override
+    public void run() {
+        for (File file : files) {
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] words = line.split("\\s*[^a-zA-Z]+\\s*");
+
+                    for (String word : words) {
+                        if (!word.isEmpty()) {
+                            if (!index.containsKey(word)) {
+                                index.put(word, new TreeSet<>());
+                            }
+                            index.get(word).add(file.getName());
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
