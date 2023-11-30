50816fc84374927b1a6b836ae40a8c76162e539d
balumatkina <balumatkinaolga@gmail.com>
2023-11-30 18:18:42 +0200

    feat(Server): add index filling at the start
    Add new thread with IndexingHandler at the start of the server.

--- code/src/server/Server.java
+++ code/src/server/Server.java
@@ -3,11 +3,18 @@ package code.src.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private Map<String, Set<String>> index;
 
     public Server(int portNumb) {
+        index = new ConcurrentHashMap<>();
         try {
+            (new Thread(() -> new IndexingHandler(index, FileHandler.getFiles("texts")).start())).start();
+
             ServerSocket serverSocket = new ServerSocket(portNumb);
             System.out.println("Server started: " + serverSocket);
             System.out.println("Waiting for client..\n");
@@ -17,12 +24,9 @@ public class Server {
                 try {
                     clientSocket = serverSocket.accept();
                     System.out.println("Client connected: " + clientSocket + "\n");
-
-                    // Multi client handle
-                    ClientHandler clientHandler = new ClientHandler(clientSocket);
+                    ClientHandler clientHandler = new ClientHandler(clientSocket, index);
                     Thread thread = new Thread(clientHandler);
                     thread.start();
-
                 } catch (Exception e) {
                     clientSocket.close();
                     e.printStackTrace();
